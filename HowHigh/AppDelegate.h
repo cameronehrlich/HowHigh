2884100073668cf71c8f8145b84b2935b76a320c
cameronehrlich <cameronehrlich@gmail.com>
2014-09-19 14:24:51 -0700

    Initial Commit

---
+++ HowHigh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HowHigh
+//
+//  Created by Cameron Ehrlich on 9/19/14.
+//  Copyright (c) 2014 Lucky Bunny LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HowHigh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HowHigh
+//
+//  Created by Cameron Ehrlich on 9/19/14.
+//  Copyright (c) 2014 Lucky Bunny LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
